8f878ac8a081e84d8dc303038018f6f09bbd8a5d
zaucy <ezekiel@robotoppression.com>
2018-10-02 21:19:02 -0700

    chmod executable for protoc-gen-dummy

---
+++ postinstall.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const DUMMY_PLUGIN_PATH = path.resolve(
+  __dirname, "../bin/protoc-gen-dummy" +
+  (process.platform == "win32" ? '.cmd' : '.sh')
+).replace(/\\/g, '/');
+
+fs.chmodSync(DUMMY_PLUGIN_PATH, '0755');
